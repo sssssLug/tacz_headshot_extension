8f5f315c336c246b1dd4b5ecdf61d945cc8c2487
sssssLug <ssssslug@gmail.com>
2025-08-25 21:23:02 +0800

    更新版本号。

--- src/main/java/com/ssssslug/tacz_headshot_extension/TACZHeadshotExtension.java
+++ src/main/java/com/ssssslug/tacz_headshot_extension/TACZHeadshotExtension.java
@@ -44,15 +44,4 @@ public class TACZHeadshotExtension {
         });
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    /*@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
-    }*/
 }
